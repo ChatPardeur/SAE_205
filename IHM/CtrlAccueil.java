beb41985976d5435296901bf23be5706ec16825e
dyburel <dylan.burel.1@etudiant.univ-rennes1.fr>
2024-06-13 14:50:11 +0200

    ajout fxml et controller

---
+++ IHM/CtrlAccueil.java
@@ -0,0 +1,31 @@
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+
+public class CtrlAccueil {
+
+    @FXML
+    private Button bnCreaZone;
+
+    @FXML
+    private Button bnListeReservAnnule;
+
+    @FXML
+    private Button bnQuitter;
+
+    @FXML
+    void clickGestionZone(ActionEvent event) {
+
+    }
+
+    @FXML
+    void clickListe(ActionEvent event) {
+
+    }
+
+    @FXML
+    void clickQuitter(ActionEvent event) {
+
+    }
+
+}
