45beeeeb0a48a14c4cd2d5b25e76aaed22972aca
dyburel <dylan.burel.1@etudiant.univ-rennes1.fr>
2024-06-16 01:02:06 +0200

    ajout dernier fxml et ctrlCreationZOne

---
+++ IHM/CtrlCreationZone.java
@@ -0,0 +1,35 @@
+package vue;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+
+public class CtrlCreationZone {
+
+    @FXML
+    private Button bnAnnuler;
+
+    @FXML
+    private Button bnCreer;
+
+    @FXML
+    private TextField nbColonne;
+
+    @FXML
+    private TextField nbLigne;
+
+    @FXML
+    private TextField nomZone;
+
+    @FXML
+    void clickAnnuler(ActionEvent event) {
+
+    }
+
+    @FXML
+    void clickCreer(ActionEvent event) {
+
+    }
+
+}
